40359bda220134b4b16ae84a4edc34496b207fce
ulugbekoqmonov <1.ulugbekoqmonov@gmail.com>
2023-07-21 11:38:47 +0500

    db migration initalized and added User, Role, Permission DbConfiguration with db migration

--- Domain/Models/Entities/Permission.cs
+++ Domain/Models/Entities/Permission.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Domain.Models.Common;
 
-namespace Domain.Models.Entities
+namespace Domain.Models.Entities;
+
+public class Permission:BaseAuditableEntity
 {
-    internal class Permission
-    {
-    }
+    public string? PermissionName { get; set; }
+
+    public virtual ICollection<Role>? Roles { get; set; }
 }
---
+++ Infrastucture/DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Application.Abstraction;
+using Domain.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastucture.DataAccess;
+
+public class ApplicationDbContext:DbContext, IApplicationDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+    {
+        
+    }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Role> Roles { get; set; }
+    public DbSet<Permission> Permissions { get; set; }
+}
